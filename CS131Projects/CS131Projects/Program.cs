e759aebf259c48d9ccc15b2004c19e5b91abb3c3
acampa2412 <65635502+acampa2412@users.noreply.github.com>
2021-02-16 20:30:14 -0700

    Add files via upload
    Module 1

---
+++ CS131Projects/CS131Projects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CS131Projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = SumOfHoursInGym(2, 2, 2, 2, 2);
+            Console.WriteLine(result);
+        }
+        public static int SumOfHoursInGym(int Monday, int Tuesday, int Wednesday, int Thursday, int Friday)
+        {
+            int result = Monday + Tuesday + Wednesday + Thursday + Friday;
+            return result;
+        }
+    }
+}
